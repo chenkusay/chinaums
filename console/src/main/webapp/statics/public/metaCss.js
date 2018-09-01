ab066f830f79aa877a494d3acfa97472a9afa3fc
gaojx <785322425@qq.com>
2018-09-01 17:20:21 +0800

    （*）整合公共js、css文件

---
+++ console/src/main/webapp/statics/public/metaCss.js
@@ -0,0 +1,15 @@
+/**
+ * Created by gaojx on 2018/9/1.
+ */
+
+document.write('<link href="statics/images/favicon.ico" rel="icon" type="image/x-icon">');
+document.write('<link href="statics/images/favicon.ico" rel="shortcut icon" type="image/x-icon">');
+document.write('<link href="statics/images/favicon.ico" rel="bookmark" type="image/x-icon">');
+
+document.write('<link href="statics/css/bootstrap.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="statics/css/font-awesome.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="statics/plugins/AdminLTE/css/AdminLTE.min.css" rel="stylesheet" type="text/css">');
+/*AdminLTE Skins. Choose a skin from the css/skins
+     folder instead of downloading all of them to reduce the load.*/
+document.write('<link href="statics/css/all-skins.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="statics/css/main.css" rel="stylesheet" type="text/css">');
---
+++ console/src/main/webapp/statics/public/metaJs.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gaojx on 2018/9/1.
+ */
+
+document.write('<script src="statics/libs/jquery.min.js"  type="text/javascript"></script>');
+document.write('<script src="statics/libs/vue.min.js"  type="text/javascript"></script>');
+document.write('<script src="statics/libs/bootstrap.min.js"  type="text/javascript"></script>');
+document.write('<script src="statics/libs/jquery.slimscroll.min.js"  type="text/javascript"></script>');
+document.write('<script src="statics/libs/fastclick.min.js"  type="text/javascript"></script>');
+document.write('<script src="statics/plugins/AdminLTE/js/app.min.js"  type="text/javascript"></script>');
+document.write('<script src="statics/libs/vue-html5-editor.js"  type="text/javascript"></script>');
+document.write('<script src="statics/libs/router.js"  type="text/javascript"></script>');
+document.write('<script src="statics/plugins/layer/layer.js"  type="text/javascript"></script>');
