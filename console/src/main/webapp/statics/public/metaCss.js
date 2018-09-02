79905de25f262e230cc10683ac3f34d9cf0312e1
gaojx <785322425@qq.com>
2018-09-02 15:46:21 +0800

    （*）修改

--- console/src/main/webapp/statics/public/metaCss.js
+++ console/src/main/webapp/statics/public/metaCss.js
@@ -1,15 +1,22 @@
 /**
  * Created by gaojx on 2018/9/1.
  */
+var strFullPath=window.document.location.href;
+var strPath=window.document.location.pathname;
+var pos=strFullPath.indexOf(strPath);
+var prePath=strFullPath.substring(0,pos);
+var postPath=strPath.substring(0,strPath.substr(1).indexOf('/')+1);
+/*http://localhost:8087/umsConsole*/
+var webPath = prePath+postPath;
 
-document.write('<link href="statics/images/favicon.ico" rel="icon" type="image/x-icon">');
-document.write('<link href="statics/images/favicon.ico" rel="shortcut icon" type="image/x-icon">');
-document.write('<link href="statics/images/favicon.ico" rel="bookmark" type="image/x-icon">');
+document.write('<link href="'+ webPath +'/statics/images/favicon.ico" rel="icon" type="image/x-icon">');
+document.write('<link href="'+ webPath +'/statics/images/favicon.ico" rel="shortcut icon" type="image/x-icon">');
+document.write('<link href="'+ webPath +'/statics/images/favicon.ico" rel="bookmark" type="image/x-icon">');
 
-document.write('<link href="statics/css/bootstrap.min.css" rel="stylesheet" type="text/css">');
-document.write('<link href="statics/css/font-awesome.min.css" rel="stylesheet" type="text/css">');
-document.write('<link href="statics/plugins/AdminLTE/css/AdminLTE.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="'+ webPath +'/statics/css/bootstrap.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="'+ webPath +'/statics/css/font-awesome.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="'+ webPath +'/statics/plugins/AdminLTE/css/AdminLTE.min.css" rel="stylesheet" type="text/css">');
 /*AdminLTE Skins. Choose a skin from the css/skins
      folder instead of downloading all of them to reduce the load.*/
-document.write('<link href="statics/css/all-skins.min.css" rel="stylesheet" type="text/css">');
-document.write('<link href="statics/css/main.css" rel="stylesheet" type="text/css">');
+document.write('<link href="'+ webPath +'/statics/css/all-skins.min.css" rel="stylesheet" type="text/css">');
+document.write('<link href="'+ webPath +'/statics/css/main.css" rel="stylesheet" type="text/css">');
--- console/src/main/webapp/statics/public/metaJs.js
+++ console/src/main/webapp/statics/public/metaJs.js
@@ -2,12 +2,12 @@
  * Created by gaojx on 2018/9/1.
  */
 
-document.write('<script src="statics/libs/jquery.min.js"  type="text/javascript"></script>');
-document.write('<script src="statics/libs/vue.min.js"  type="text/javascript"></script>');
-document.write('<script src="statics/libs/bootstrap.min.js"  type="text/javascript"></script>');
-document.write('<script src="statics/libs/jquery.slimscroll.min.js"  type="text/javascript"></script>');
-document.write('<script src="statics/libs/fastclick.min.js"  type="text/javascript"></script>');
-document.write('<script src="statics/plugins/AdminLTE/js/app.min.js"  type="text/javascript"></script>');
-document.write('<script src="statics/libs/vue-html5-editor.js"  type="text/javascript"></script>');
-document.write('<script src="statics/libs/router.js"  type="text/javascript"></script>');
-document.write('<script src="statics/plugins/layer/layer.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/jquery.min.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/vue.min.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/bootstrap.min.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/jquery.slimscroll.min.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/fastclick.min.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/plugins/AdminLTE/js/app.min.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/vue-html5-editor.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/libs/router.js"  type="text/javascript"></script>');
+document.write('<script src="'+ webPath +'/statics/plugins/layer/layer.js"  type="text/javascript"></script>');
